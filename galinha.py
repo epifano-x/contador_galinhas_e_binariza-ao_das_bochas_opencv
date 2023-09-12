fb7af9b00e546a701acbbe739166a837ddaaf9c6
BRUNO ROGLIN <49683237+epifano-x@users.noreply.github.com>
2023-09-12 04:11:54 -0300

    galinhas separadas por bolas

--- galinha.py
+++ galinha.py
@@ -1,41 +1,84 @@
-import numpy as np
 import cv2
+import numpy as np
 
-# Carregue a imagem e converta para escala de cinza
-img = cv2.imread('galinha1.png')
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Aplicar um filtro de desfoque Gaussiano para reduzir o ruído
-blurred_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
 
-# Aplicar o operador Sobel
-sobelX = cv2.Sobel(blurred_img, cv2.CV_64F, 1, 0)
-sobelY = cv2.Sobel(blurred_img, cv2.CV_64F, 0, 1)
+# Carrega as imagens
+imagem_galinha = cv2.imread('galinha.png')
+imagem_galinhafade = cv2.imread('galinhafade.png', -1)  # Carrega a imagem com o canal de transparência
 
-# Calcule a magnitude do gradiente (Sobel)
-sobel_magnitude = np.sqrt(sobelX**2 + sobelY**2)
+# Redimensiona a imagem da máscara para o tamanho da imagem da galinha
+imagem_galinhafade = cv2.resize(imagem_galinhafade, (imagem_galinha.shape[1], imagem_galinha.shape[0]))
 
-# Aplicar um threshold na imagem do gradiente
-_, thresholded_img = cv2.threshold(sobel_magnitude, 20, 255, cv2.THRESH_BINARY)
+# Separa o canal de opacidade da imagem da máscara
+canal_alpha = imagem_galinhafade[:, :, 3]
 
-# Defina as coordenadas da região de interesse (ROI)
-x1, x2, y1, y2 = 100, 300, 150, 350  # Substitua esses valores pelos valores desejados
+# Remove o canal de opacidade da imagem da máscara
+imagem_galinhafade = imagem_galinhafade[:, :, 0:3]
 
-# Defina a região de interesse (ROI)
-roi = thresholded_img[y1:y2, x1:x2]
+# Aplica a máscara com opacidade na imagem da galinha
+imagem_resultante = cv2.addWeighted(imagem_galinha, 0.5, imagem_galinhafade, 2, 0)
 
-# Converta a imagem da ROI para o formato CV_8UC1
-roi = np.uint8(roi)
+# Converte a imagem resultante em escala de cinza
+imagem_cinza = cv2.cvtColor(imagem_resultante, cv2.COLOR_BGR2GRAY)
 
-# Encontre e conte os contornos na ROI
-contours, _ = cv2.findContours(roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Cria uma máscara radial gaussiana invertida
+altura, largura = imagem_cinza.shape
+y, x = np.ogrid[-altura/2:altura/2, -largura/2:largura/2]
+raio = np.sqrt(x**2 + y**2)
+sigma = largura / 3.0  # Ajuste o valor de sigma conforme necessário para controlar o efeito
+mascara_radial_invertida = 1.33 - np.exp(-raio**2 / (2 * sigma**2))
 
-# Conte o número de "galinhas" na região
-num_galinhas = len(contours)
+# Ajusta o escurecimento tornando a máscara mais opaca
+fator_escurecimento = 2.5  # Ajuste conforme necessário
+mascara_ajustada = mascara_radial_invertida * fator_escurecimento
 
-# Exiba o resultado
-print(f'Número de galinhas na região selecionada: {num_galinhas}')
+# Aplica a máscara radial ajustada à imagem em tons de cinza
+imagem_ajustada = imagem_cinza * mascara_ajustada
 
-# Exiba a imagem de saída com as regiões de interesse
-cv2.imshow("Sobel", thresholded_img)
+# Converte a imagem ajustada para o tipo CV_8U
+imagem_ajustada = cv2.convertScaleAbs(imagem_ajustada)
+
+# Aplica o operador Top-Hat
+kernel = np.ones((250, 250), np.uint8)
+imagem_tophat = cv2.morphologyEx(imagem_ajustada, cv2.MORPH_TOPHAT, kernel)
+
+# Normaliza a imagem para aumentar o contraste
+imagem_tophat_normalizada = cv2.normalize(imagem_tophat, None, 0, 255, cv2.NORM_MINMAX)
+
+# Cria uma lista vazia para rastrear as posições das bolas brancas
+posicoes_anteriores = []
+
+# Cria bolas brancas nos locais apropriados
+threshold = 200  # Valor de intensidade para considerar um pixel como claro
+imagem_preta = np.zeros_like(imagem_tophat_normalizada)
+# Função para criar uma bola branca no local de um pixel claro
+def criar_bola_branca(imagem_tophat_normalizada, threshold, posicoes_anteriores):
+    altura, largura = imagem_tophat_normalizada.shape
+    cor_branca = (255, 255, 255)  # Cor branca
+    raio_bola = 10
+    threshold = 70
+    for y in range(altura):
+        for x in range(largura):
+            if imagem_tophat_normalizada[y, x] >= threshold:  # Verifica se o pixel é claro o suficiente
+                # Verifica se a posição já foi usada
+                posicao_atual = (x, y)
+                criar = True
+                for posicao_anterior in posicoes_anteriores:
+                    if np.linalg.norm(np.array(posicao_atual) - np.array(posicao_anterior)) < raio_bola + 25:
+                        criar = False
+                        break
+
+                if criar:
+                    cv2.circle(imagem_preta, (x, y), raio_bola, cor_branca, -1)  # Cria uma bola branca no local
+                    posicoes_anteriores.append(posicao_atual)
+                    
+
+criar_bola_branca(imagem_tophat_normalizada, threshold, posicoes_anteriores)
+
+
+# Exibe a imagem com as bolas brancas
+cv2.imshow('Imagem com Bolas Brancas', imagem_tophat_normalizada)
+cv2.imshow('Imagem com Bolas preta', imagem_preta)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
--- sobel.py
+++ sobel.py
@@ -1,34 +1,51 @@
-import numpy as np
 import cv2
+import numpy as np
 
-# Carregue a imagem 'galinha.png'
-img = cv2.imread('galinha.png')
+# Carrega as imagens
+imagem_galinha = cv2.imread('galinha.png')
+imagem_galinhafade = cv2.imread('galinhafade.png', -1)  # Carrega a imagem com o canal de transparência
 
-# Converta a imagem para tons de cinza
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Redimensiona a imagem da máscara para o tamanho da imagem da galinha
+imagem_galinhafade = cv2.resize(imagem_galinhafade, (imagem_galinha.shape[1], imagem_galinha.shape[0]))
 
-# Aplique o filtro de Sobel para detectar bordas
-sobelX = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0)
-sobelY = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1)
+# Separa o canal de opacidade da imagem da máscara
+canal_alpha = imagem_galinhafade[:, :, 3]
 
-# Converta os resultados para valores absolutos e converta para tipo uint8
-sobelX = np.uint8(np.absolute(sobelX))
-sobelY = np.uint8(np.absolute(sobelY))
+# Remove o canal de opacidade da imagem da máscara
+imagem_galinhafade = imagem_galinhafade[:, :, 0:3]
 
-# Combine as imagens de borda usando bitwise_or
-sobel = cv2.bitwise_or(sobelX, sobelY)
+# Aplica a máscara com opacidade na imagem da galinha
+imagem_resultante = cv2.addWeighted(imagem_galinha, 0.5, imagem_galinhafade, 2, 0)
 
-# Aplique uma operação de dilatação para preencher as regiões com margens
-kernel = np.ones((5, 5), np.uint8)
-sobel_dilated = cv2.dilate(sobel, kernel, iterations=1)
+# Converte a imagem resultante em escala de cinza
+imagem_cinza = cv2.cvtColor(imagem_resultante, cv2.COLOR_BGR2GRAY)
 
-# Empilhe as imagens para exibição
-resultado = np.vstack([
-    np.hstack([img_gray, sobel]),
-    np.hstack([sobel_dilated, sobel])
-])
+# Cria uma máscara radial gaussiana invertida
+altura, largura = imagem_cinza.shape
+y, x = np.ogrid[-altura/2:altura/2, -largura/2:largura/2]
+raio = np.sqrt(x**2 + y**2)
+sigma = largura / 3.0  # Ajuste o valor de sigma conforme necessário para controlar o efeito
+mascara_radial_invertida = 1.33 - np.exp(-raio**2 / (2 * sigma**2))
 
-# Exiba a imagem resultante
-cv2.imshow("Sobel com Preenchimento", resultado)
+# Ajusta o escurecimento tornando a máscara mais opaca
+fator_escurecimento = 2.5  # Ajuste conforme necessário
+mascara_ajustada = mascara_radial_invertida * fator_escurecimento
+
+# Aplica a máscara radial ajustada à imagem em tons de cinza
+imagem_ajustada = imagem_cinza * mascara_ajustada
+
+# Converte a imagem ajustada para o tipo CV_8U
+imagem_ajustada = cv2.convertScaleAbs(imagem_ajustada)
+
+# Exibe a imagem com o centro mais escuro e opaco
+cv2.imshow('Imagem com Centro Mais Escuro e Opaco', imagem_ajustada)
+
+# Aplica o operador Top-Hat
+kernel = np.ones((250, 250), np.uint8)
+imagem_tophat = cv2.morphologyEx(imagem_ajustada, cv2.MORPH_TOPHAT, kernel)
+
+# Exibe a imagem após a aplicação do operador Top-Hat
+cv2.imshow('Imagem após o Operador Top-Hat', imagem_tophat)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
